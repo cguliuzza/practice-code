fada56d2c9f799d4a8d2fd21d3e26c72f445f2a1
christinaguliuzza <christina.guliuzza@gmail.com>
2021-10-15 20:36:14 -0700

    plus-minus hackerRank problem 1

---
+++ LeetCode/Ruby/plus-minus.rb
@@ -0,0 +1,31 @@
+#!/bin/ruby
+
+require 'json'
+require 'stringio'
+
+#
+# Complete the 'plusMinus' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def plusMinus(arr)
+    # Write your code here
+
+
+
+#  Did not figure this problem out
+    binding.pry
+    length = arr.length
+
+
+
+
+
+end
+
+n = gets.strip.to_i
+
+arr = gets.rstrip.split.map(&:to_i)
+
+plusMinus arr
