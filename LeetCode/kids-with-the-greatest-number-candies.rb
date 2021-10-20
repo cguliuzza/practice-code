38d008c8fba0e8cdab57814a1a63b2264f4a5654
christinaguliuzza <christina.guliuzza@gmail.com>
2021-10-20 07:13:44 -0700

    flatiron phase 4 algo practice

---
+++ LeetCode/kids-with-the-greatest-number-candies.rb
@@ -0,0 +1,15 @@
+# @param {Integer[]} candies
+# @param {Integer} extra_candies
+# @return {Boolean[]}
+require "pry"
+
+def kids_with_candies(candies, extra_candies)
+    most_candies = candies.max
+end
+
+# sort candies array from least to greatest to determine the largest integer
+# map through the original array to see if the element + extracandies is greater than the largest integer
+
+# if greater or equal to render true
+    
+# else render false
---
+++ LeetCode/shuffle-the-array.rb
@@ -0,0 +1,27 @@
+# @param {Integer[]} nums
+# @param {Integer} n
+# @return {Integer[]}
+def shuffle(nums, n)
+    
+end
+
+# iterate over the array
+
+# x = 0
+# n = arr.length - (arr.length/2)
+
+# arr[0], arr[n], arr[0 + 1], arr[n + 1]
+
+# 0 will always be the index of the first element
+# n will always be the index value of the second element
+# 0 + 1 will always be the index of the third element
+# n + 1 will always be the index value of the fourth element
+
+# n * 2 is the length of the array (the total number of elements is n * 2)
+
+# split the array in half??
+# the value of n is always half of the array length
+# arr1 = [arr[0], arr[1], arr[2]]
+# arr2 = arr[]
+
+TIME AND SPACE COMPLEXITY
